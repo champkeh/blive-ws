4d30e397c90b24b1e3eccc65e480f6f20d02e694
champkeh <champion.ge@gmail.com>
2022-07-10 18:07:53 +0800

    添加代理服务器，方便本地调试接口

---
+++ proxy/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const app = express()
+
+const PORT = 3000
+const HOST = "localhost"
+const API_SERVICE_URL = "https://api.live.bilibili.com"
+
+// Logging
+app.use(morgan('dev'))
+
+// Proxy endpoints
+app.use('/proxy', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    headers: {
+        referer: 'https://live.bilibili.com/'
+    },
+    pathRewrite: {
+        [`^/proxy`]: '',
+    }
+}))
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`)
+})
